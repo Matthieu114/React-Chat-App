aa34aca5aeec5fd2b269f799291d1f28471d7e94
mout <bricebenjam1@gmail.com>
2021-11-22 15:04:59 +0100

    a

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,7 @@
 /** @jsxImportSource @emotion/react */
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 // Local
 import Oups from './Oups';
-import Footer from './Footer';
-import Header from './Header';
 import Main from './Main';
 import Login from './Login';
 import Context from './Context';
@@ -11,40 +9,40 @@ import Context from './Context';
 import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 
 const styles = {
-  root: {
-    boxSizing: 'border-box',
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: '#565E71'
-  }
+	root: {
+		boxSizing: 'border-box',
+		display: 'flex',
+		flexDirection: 'column',
+		backgroundColor: '#565E71'
+	}
 };
 
 export default function App() {
-  const location = useLocation();
-  const { oauth } = useContext(Context);
-  const gochannels = (
-    <Navigate
-      to={{
-        pathname: '/channels',
-        state: { from: location }
-      }}
-    />
-  );
-  const gohome = (
-    <Navigate
-      to={{
-        pathname: '/',
-        state: { from: location }
-      }}
-    />
-  );
-  return (
-    <div className='App' css={styles.root}>
-      <Routes>
-        <Route exact path='/' element={oauth ? gochannels : <Login />} />
-        <Route path='/channels/*' element={oauth ? <Main /> : gohome} />
-        <Route path='/Oups' element={<Oups />} />
-      </Routes>
-    </div>
-  );
+	const location = useLocation();
+	const { oauth } = useContext(Context);
+	const gochannels = (
+		<Navigate
+			to={{
+				pathname: '/channels',
+				state: { from: location }
+			}}
+		/>
+	);
+	const gohome = (
+		<Navigate
+			to={{
+				pathname: '/',
+				state: { from: location }
+			}}
+		/>
+	);
+	return (
+		<div className='App' css={styles.root}>
+			<Routes>
+				<Route exact path='/' element={oauth ? gochannels : <Login />} />
+				<Route path='/channels/*' element={oauth ? <Main /> : gohome} />
+				<Route path='/Oups' element={<Oups />} />
+			</Routes>
+		</div>
+	);
 }
--- front-end/src/Channels.js
+++ front-end/src/Channels.js
@@ -11,121 +11,113 @@ import { useNavigate } from 'react-router-dom';
 import Discussions from './Discussions';
 
 const styles = {
-  root: {
-    minWidth: '200px',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'start',
-    backgroundColor: 'white'
-  },
-  list: {
-    overflow: 'auto'
-  },
-  channel: {
-    padding: '1.2rem',
-    whiteSpace: 'nowrap',
-    ':hover': {
-      backgroundColor: 'lightgrey'
-    }
-  }
+	root: {
+		minWidth: '200px',
+		height: '100vh',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'start',
+		backgroundColor: 'white'
+	},
+	list: {
+		overflow: 'auto'
+	},
+	channel: {
+		padding: '1.2rem',
+		whiteSpace: 'nowrap',
+		':hover': {
+			backgroundColor: 'lightgrey'
+		}
+	}
 };
 
 export default function Channels() {
-  const { oauth, channels, setChannels } = useContext(Context);
-  const [isShown, setIsShown] = useState(false);
-  const [currentChannel, setCurrentChannel] = useState('');
-  const navigate = useNavigate();
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const { data: channels } = await axios.get(
-          'http://localhost:3001/channels',
-          {
-            headers: {
-              Authorization: `Bearer ${oauth.access_token}`
-            }
-          }
-        );
-        setChannels(channels);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetch();
-  }, [oauth, setChannels]);
+	const { oauth, channels, setChannels } = useContext(Context);
+	const [isShown, setIsShown] = useState(false);
+	const [currentChannel, setCurrentChannel] = useState('');
+	const navigate = useNavigate();
+	useEffect(() => {
+		const fetch = async () => {
+			try {
+				const { data: channels } = await axios.get(
+					'http://localhost:3001/channels',
+					{
+						headers: {
+							Authorization: `Bearer ${oauth.access_token}`
+						}
+					}
+				);
+				setChannels(channels);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		fetch();
+	}, [oauth, setChannels]);
 
-  const fetchChannels = async () => {
-    const { data: channels } = await axios.get(
-      'http://localhost:3001/channels',
-      {
-        headers: {
-          Authorization: `Bearer ${oauth.access_token}`
-        }
-      }
-    );
-    setChannels(channels);
-  };
+	const fetchChannels = async () => {
+		const { data: channels } = await axios.get(
+			'http://localhost:3001/channels',
+			{
+				headers: {
+					Authorization: `Bearer ${oauth.access_token}`
+				}
+			}
+		);
+		setChannels(channels);
+	};
 
-  const deleteChannel = async (channel) => {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${oauth.access_token}`
-      },
-      params: {
-        name: channel.name,
-        id: channel.id
-      }
-    };
-    const { data: channels } = await axios.delete(
-      `http://localhost:3001/channels/${channel.id}`,
-      { config }
-    );
+	const deleteChannel = async (channel) => {
+		const config = {
+			headers: {
+				Authorization: `Bearer ${oauth.access_token}`
+			},
+			data: {
+				name: channel.name,
+				id: channel.id
+			}
+		};
+		const { data: channels } = await axios.delete(
+			`http://localhost:3001/channels/${channel.id}`,
+			{ config }
+		);
 
-    fetchChannels(channels);
-  };
+		fetchChannels(channels);
+	};
 
-  return (
-    <div css={styles.root}>
-      <Discussions />
-      <ul css={styles.list}>
-        {/* <li css={styles.channel}>
-          <Link to='/channels' component={RouterLink}>
-            Welcome
-          </Link>
-        </li> */}
-        {channels.map((channel, i) => (
-          <li
-            key={i}
-            css={styles.channel}
-            onMouseEnter={(e) => {
-              setIsShown(true);
-              setCurrentChannel(e.target.innerText);
-            }}
-            onMouseLeave={(e) => setIsShown(false)}
-            onClick={(e) => {
-              e.preventDefault();
-              navigate(`/channels/${channel.id}`);
-            }}
-          >
-            <Link
-              sx={{ textDecoration: 'none', color: 'black' }}
-              href={`/channels/${channel.id}`}
-            >
-              {channel.name}
-            </Link>
-            {currentChannel === channel.name.trim() && isShown && (
-              <IconButton
-                style={{ float: 'right', margin: '-15px' }}
-                color='info'
-                onClick={() => deleteChannel(channel)}
-              >
-                <RemoveOutlinedIcon />
-              </IconButton>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div css={styles.root}>
+			<Discussions />
+			<ul css={styles.list}>
+				{channels.map((channel, i) => (
+					<li
+						key={i}
+						css={styles.channel}
+						onMouseEnter={(e) => {
+							setIsShown(true);
+							setCurrentChannel(e.target.innerText);
+						}}
+						onMouseLeave={(e) => setIsShown(false)}
+						onClick={(e) => {
+							e.preventDefault();
+							navigate(`/channels/${channel.id}`);
+						}}>
+						<Link
+							sx={{ textDecoration: 'none', color: 'black' }}
+							href={`/channels/${channel.id}`}>
+							{channel.name}
+						</Link>
+						{currentChannel === channel.name.trim() && isShown && (
+							<IconButton
+								style={{ float: 'right', margin: '-15px' }}
+								color='info'
+								onClick={() => deleteChannel(channel)}>
+								<RemoveOutlinedIcon />
+							</IconButton>
+						)}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
