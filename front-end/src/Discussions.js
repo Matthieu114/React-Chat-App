67a4eeb9cb61ee5083483c719e1440d155b4112d
Matthieu Denis <mdenis300@gmail.com>
2021-11-24 18:16:41 +0100

    css: added margin on discussions for more avatar visiblity

--- front-end/src/Discussions.js
+++ front-end/src/Discussions.js
@@ -5,35 +5,36 @@ import AddChannel from './channel/AddChannel.js';
 import AvatarProfil from './Avatar';
 
 const styles = {
-	root: {
-		marginTop: '0px',
-		position: 'sticky',
-		top: '0',
-		backgroundColor: 'White'
-	},
-	text: {
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		marginBottom: '-5px',
-		color: 'black',
-		clear: 'both'
-	},
-	avatar: {
-		marginRight: '10px'
-	}
+  root: {
+    marginTop: '0px',
+    position: 'sticky',
+    top: '0',
+    backgroundColor: 'White'
+  },
+  text: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '-5px',
+    color: 'black',
+    clear: 'both'
+  },
+  avatar: {
+    marginRight: '10px'
+  }
 };
 
 const Discussions = () => {
-	return (
-		<div css={styles.root}>
-			<h2 css={styles.text}>
-				<AvatarProfil sx={styles.avatar} /> Discussions <AddChannel />
-			</h2>
+  return (
+    <div css={styles.root}>
+      <h2 css={styles.text}>
+        <AvatarProfil sx={styles.avatar} />{' '}
+        <p style={{ margin: '0 15px' }}>Discussions</p> <AddChannel />
+      </h2>
 
-			<SearchBar />
-		</div>
-	);
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Discussions;
