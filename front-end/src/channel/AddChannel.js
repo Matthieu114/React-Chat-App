fe1c265363e15d874a308621052c49d8bee47a93
Matthieu Denis <mdenis300@gmail.com>
2021-11-25 17:52:47 +0100

    feat: added edit messages

--- front-end/src/channel/AddChannel.js
+++ front-end/src/channel/AddChannel.js
@@ -1,10 +1,10 @@
 import {
-	Button,
-	Box,
-	Typography,
-	Modal,
-	TextField,
-	IconButton
+  Button,
+  Box,
+  Typography,
+  Modal,
+  TextField,
+  IconButton
 } from '@mui/material';
 import axios from 'axios';
 //Context
@@ -13,101 +13,116 @@ import { useContext, useState } from 'react';
 import AddRounded from '@mui/icons-material/AddRounded';
 
 const styles = {
-	box: {
-		position: 'absolute',
-		top: '50%',
-		left: '50%',
-		transform: 'translate(-50%, -50%)',
-		width: 'max-content',
-		bgcolor: 'white',
-		p: 4,
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center'
-	},
-	box2: {
-		display: 'flex',
-		alignItems: 'center',
-		color: 'white'
-	}
+  box: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 'max-content',
+    bgcolor: 'white',
+    p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  box2: {
+    justifyContent: 'center',
+    color: 'white'
+  }
 };
 
 const AddChannel = () => {
-	const [open, setOpen] = useState(false);
-	const handleClose = () => setOpen(false);
-	const handleOpen = () => setOpen(true);
-	const { setChannels, oauth } = useContext(Context);
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+  const { setChannels, oauth } = useContext(Context);
 
-	const fetchChannels = async () => {
-		const { data: channels } = await axios.get(
-			'http://localhost:3001/channels',
-			{
-				headers: {
-					Authorization: `Bearer ${oauth.access_token}`
-				}
-			}
-		);
-		setChannels(channels);
-	};
+  const fetchChannels = async () => {
+    const { data: channels } = await axios.get(
+      'http://localhost:3001/channels',
+      {
+        headers: {
+          Authorization: `Bearer ${oauth.access_token}`
+        }
+      }
+    );
+    setChannels(channels);
+  };
 
-	const onSubmit = async () => {
-		const { data: channels } = await axios.post(
-			`http://localhost:3001/channels`,
-			{
-				name: document.getElementById('newChannel').value
-			}
-		);
-		fetchChannels(channels);
-		handleClose();
-	};
+  const onSubmit = async () => {
+    const { data: channels } = await axios.post(
+      `http://localhost:3001/channels`,
+      {
+        name: document.getElementById('newChannel').value
+      }
+    );
+    fetchChannels(channels);
+    handleClose();
+  };
 
-	const onKeyPress = ({ nativeEvent: { key: keyValue } }) => {
-		if (keyValue === 'Enter') onSubmit();
-	};
+  const onKeyPress = ({ nativeEvent: { key: keyValue } }) => {
+    if (keyValue === 'Enter') onSubmit();
+  };
 
-	return (
-		<div>
-			<IconButton
-				color='info'
-				variant='outlined'
-				marginTop='300px'
-				onClick={handleOpen}>
-				<AddRounded />
-			</IconButton>
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby='modal-modal-title'
-				aria-describedby='modal-modal-description'>
-				<Box sx={styles.box}>
-					<Typography
-						color='black'
-						id='modal-modal-title'
-						variant='h6'
-						component='h6'>
-						Create a new Channel
-					</Typography>
-					<Box sx={styles.box2}>
-						<TextField
-							size='small'
-							id='newChannel'
-							label='Name of Channel'
-							variant='standard'
-							name='newChannel'
-							onKeyPress={onKeyPress}
-						/>
-						<Button
-							variant='outlined'
-							size='small'
-							sx={{ marginTop: '15px', marginLeft: '10px' }}
-							onClick={onSubmit}>
-							ADD
-						</Button>
-					</Box>
-				</Box>
-			</Modal>
-		</div>
-	);
+  return (
+    <div>
+      <IconButton
+        color='info'
+        variant='outlined'
+        marginTop='300px'
+        onClick={handleOpen}
+      >
+        <AddRounded />
+      </IconButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={styles.box}>
+          <Box sx={styles.box2}>
+            <TextField
+              autoFocus
+              fullWidth
+              id='newChannel'
+              label='New Channel Name'
+              variant='standard'
+              name='newChannel'
+              onKeyPress={onKeyPress}
+              helperText='New Channel Name'
+            />
+            <Button
+              variant='outlined'
+              fullWidth
+              sx={{
+                marginTop: '15px',
+                marginLeft: '10px',
+                justifyContent: 'center'
+              }}
+              onClick={onSubmit}
+              color='info'
+            >
+              ADD
+            </Button>
+            <Button
+              variant='outlined'
+              fullWidth
+              sx={{
+                marginTop: '15px',
+                marginLeft: '10px',
+                justifyContent: 'center'
+              }}
+              color='info'
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
 };
 
 export default AddChannel;
--- front-end/src/channel/List.js
+++ front-end/src/channel/List.js
@@ -1,14 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import {
-	forwardRef,
-	useImperativeHandle,
-	useLayoutEffect,
-	useRef,
-	useState
+  forwardRef,
+  useImperativeHandle,
+  useLayoutEffect,
+  useRef,
+  useState
 } from 'react';
 // Layout
 import { useTheme } from '@mui/styles';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Button, TextField } from '@mui/material';
 // Markdown
 import { unified } from 'unified';
 import markdown from 'remark-parse';
@@ -20,143 +21,237 @@ import calendar from 'dayjs/plugin/calendar';
 import updateLocale from 'dayjs/plugin/updateLocale';
 //local
 import MessageChanger from './MessageChanger';
+import axios from 'axios';
+import Context from '../Context';
+import { useContext } from 'react';
 
 dayjs.extend(calendar);
 dayjs.extend(updateLocale);
 dayjs.updateLocale('en', {
-	calendar: {
-		sameElse: 'DD/MM/YYYY hh:mm A'
-	}
+  calendar: {
+    sameElse: 'DD/MM/YYYY hh:mm A'
+  }
 });
 
 const useStyles = (theme) => ({
-	root: {
-		position: 'relative',
-		flex: '1 1 auto',
-		overflow: 'auto',
-		'& ul': {
-			margin: 0,
-			padding: 0,
-			textIndent: 0,
-			listStyleType: 0
-		}
-	},
-	message: {
-		padding: '.2rem .5rem',
-		':hover': {
-			backgroundColor: 'rgba(255,255,255,.05)'
-		}
-	},
-	fabWrapper: {
-		position: 'absolute',
-		right: 0,
-		top: 0,
-		width: '50px'
-	},
-	fab: {
-		position: 'fixed !important',
-		top: '0',
-		width: '50px'
-	}
+  root: {
+    position: 'relative',
+    flex: '1 1 auto',
+    overflow: 'auto',
+    '& ul': {
+      margin: 0,
+      padding: 0,
+      textIndent: 0,
+      listStyleType: 0
+    }
+  },
+  message: {
+    padding: '.2rem .5rem',
+    ':hover': {
+      backgroundColor: 'rgba(255,255,255,.05)'
+    }
+  },
+  fabWrapper: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    width: '50px'
+  },
+  fab: {
+    position: 'fixed !important',
+    top: '0',
+    width: '50px'
+  },
+
+  edit: {
+    display: 'flex',
+    alignItems: 'center'
+  }
 });
 
 const LiMessage = ({ message, i, value, channel, setMessages }) => {
-	const [isShown, setIsShown] = useState(false);
-	const [anchorEl, setAnchorEl] = useState(null);
-	const styles = useStyles(useTheme());
-	const open = Boolean(anchorEl);
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-		setIsShown(false);
-	};
-	return (
-		<li
-			key={i}
-			css={styles.message}
-			onMouseEnter={() => setIsShown(true)}
-			onMouseLeave={() => setIsShown(false)}>
-			<p>
-				<span>{message.author}</span>
-				{' - '}
-				<span>{dayjs().calendar(message.creation)}</span>
-				{isShown && (
-					<MoreVertIcon
-						color='info'
-						onClick={handleClick}
-						style={{ margin: '-10px 0px' }}
-					/>
-				)}
-				<MessageChanger
-					anchorEl={anchorEl}
-					open={open}
-					handleClose={handleClose}
-					message={message}
-					channel={channel}
-					setMessages={setMessages}
-				/>
-			</p>
-			<div dangerouslySetInnerHTML={{ __html: value }}></div>
-		</li>
-	);
+  const [isShown, setIsShown] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [editOpen, setEditOpen] = useState(false);
+  const { oauth } = useContext(Context);
+  const styles = useStyles(useTheme());
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+    setIsShown(false);
+  };
+
+  const toggleEdit = () => {
+    setEditOpen(!editOpen);
+  };
+
+  const closeEdit = () => {
+    setEditOpen(false);
+  };
+
+  const fetchMessages = async () => {
+    const { data: messages } = await axios.get(
+      `http://localhost:3001/channels/${channel.id}/messages`,
+      {
+        headers: {
+          Authorization: `Bearer ${oauth.access_token}`
+        }
+      }
+    );
+    setMessages(messages);
+  };
+
+  const editMessage = async (message, channel) => {
+    const { data: messages } = await fetch(
+      `http://localhost:3001/channels/${channel.id}/messages/${message.creation}`,
+      {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${oauth.access_token}`,
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          author: oauth.email,
+          content: document.getElementById('editedMessage').value
+        })
+      }
+    );
+
+    fetchMessages(messages);
+  };
+
+  const onKeyPress = ({ nativeEvent: { key: keyValue } }) => {
+    if (keyValue === 'Enter') {
+      editMessage(message, channel);
+      toggleEdit();
+    }
+  };
+
+  return (
+    <li
+      key={i}
+      css={styles.message}
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      <p>
+        <span>{message.author}</span>
+        {' - '}
+        <span>{dayjs().calendar(message.creation)}</span>
+        {isShown && oauth.email === message.author ? (
+          <MoreVertIcon
+            color='info'
+            onClick={handleClick}
+            style={{ margin: '-10px 0px' }}
+          />
+        ) : (
+          console.log('not yourmessage')
+        )}
+        <MessageChanger
+          anchorEl={anchorEl}
+          open={open}
+          handleClose={handleClose}
+          message={message}
+          channel={channel}
+          setMessages={setMessages}
+          editOpen={toggleEdit}
+          fetchMessages={fetchMessages}
+          oauth={oauth}
+        />
+      </p>
+      {!editOpen ? (
+        <div dangerouslySetInnerHTML={{ __html: value }}></div>
+      ) : (
+        <div css={styles.edit}>
+          <TextField
+            autoFocus
+            type='text'
+            placeholder={message.content}
+            id='editedMessage'
+            onKeyPress={(e) => {
+              onKeyPress(e);
+            }}
+            variant='standard'
+            color='info'
+            sx={{
+              input: { color: 'white' }
+            }}
+          ></TextField>
+          <Button
+            onClick={() => {
+              toggleEdit();
+              editMessage(message, channel);
+            }}
+            color='info'
+          >
+            confirm
+          </Button>
+          <Button onClick={closeEdit} color='info'>
+            Cancel
+          </Button>
+        </div>
+      )}
+    </li>
+  );
 };
 
 export default forwardRef(
-	({ messages, onScrollDown, channel, setMessages }, ref) => {
-		const styles = useStyles(useTheme());
-		// Expose the `scroll` action
-		useImperativeHandle(ref, () => ({
-			scroll: scroll
-		}));
-		const rootEl = useRef(null);
-		const scrollEl = useRef(null);
+  ({ messages, onScrollDown, channel, setMessages }, ref) => {
+    const styles = useStyles(useTheme());
+    // Expose the `scroll` action
+    useImperativeHandle(ref, () => ({
+      scroll: scroll
+    }));
+    const rootEl = useRef(null);
+    const scrollEl = useRef(null);
 
-		const scroll = () => {
-			scrollEl.current.scrollIntoView();
-		};
-		// See https://dev.to/n8tb1t/tracking-scroll-position-with-react-hooks-3bbj
-		const throttleTimeout = useRef(null); // react-hooks/exhaustive-deps
+    const scroll = () => {
+      scrollEl.current.scrollIntoView();
+    };
+    // See https://dev.to/n8tb1t/tracking-scroll-position-with-react-hooks-3bbj
+    const throttleTimeout = useRef(null); // react-hooks/exhaustive-deps
 
-		useLayoutEffect(() => {
-			const rootNode = rootEl.current; // react-hooks/exhaustive-deps
-			const handleScroll = () => {
-				if (throttleTimeout.current === null) {
-					throttleTimeout.current = setTimeout(() => {
-						throttleTimeout.current = null;
-						const { scrollTop, offsetHeight, scrollHeight } = rootNode; // react-hooks/exhaustive-deps
-						onScrollDown(scrollTop + offsetHeight < scrollHeight);
-					}, 200);
-				}
-			};
+    useLayoutEffect(() => {
+      const rootNode = rootEl.current; // react-hooks/exhaustive-deps
+      const handleScroll = () => {
+        if (throttleTimeout.current === null) {
+          throttleTimeout.current = setTimeout(() => {
+            throttleTimeout.current = null;
+            const { scrollTop, offsetHeight, scrollHeight } = rootNode; // react-hooks/exhaustive-deps
+            onScrollDown(scrollTop + offsetHeight < scrollHeight);
+          }, 200);
+        }
+      };
 
-			handleScroll();
-			rootNode.addEventListener('scroll', handleScroll);
-			return () => rootNode.removeEventListener('scroll', handleScroll);
-		});
-		return (
-			<div css={styles.root} ref={rootEl}>
-				<ul>
-					{messages.map((message, i) => {
-						const { value } = unified()
-							.use(markdown)
-							.use(remark2rehype)
-							.use(html)
-							.processSync(message.content);
-						return (
-							<LiMessage
-								message={message}
-								i={i}
-								value={value}
-								channel={channel}
-								setMessages={setMessages}
-							/>
-						);
-					})}
-				</ul>
-				<div ref={scrollEl} />
-			</div>
-		);
-	}
+      handleScroll();
+      rootNode.addEventListener('scroll', handleScroll);
+      return () => rootNode.removeEventListener('scroll', handleScroll);
+    });
+    return (
+      <div css={styles.root} ref={rootEl}>
+        <ul>
+          {messages.map((message, i) => {
+            const { value } = unified()
+              .use(markdown)
+              .use(remark2rehype)
+              .use(html)
+              .processSync(message.content);
+            return (
+              <LiMessage
+                message={message}
+                i={i}
+                value={value}
+                channel={channel}
+                setMessages={setMessages}
+              />
+            );
+          })}
+        </ul>
+        <div ref={scrollEl} />
+      </div>
+    );
+  }
 );
